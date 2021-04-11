96b6d5eda64058c531ef9d5fda42caffa5984236
Amélia Alice <alice.cardoso10@gmail.com>
2021-04-10 22:19:58 -0300

    :tada: adding first microservice (product service)

---
+++ product-service/src/main/java/com/microservice/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.microservice.productservice.repository;
+
+import com.microservice.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
