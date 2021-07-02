5ec67db6a47ae82a1c255b42267ca4bc0d8bccc2
Amélia Alice <alice.cardoso10@gmail.com>
2021-07-02 20:46:20 -0300

    :construction: starting establishing interservice communication and circuit breaker configuration

---
+++ order-service/src/main/java/com/microservice/orderservice/client/InventoryClient.java
@@ -0,0 +1,11 @@
+package com.microservice.orderservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryClient {
+    @GetMapping("/api/inventory/{skuCode}")
+    Boolean checkStock(@PathVariable String skuCode);
+}
