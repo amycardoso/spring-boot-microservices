c6b404ce189ca6b4230bc2922fa2bd0af3dfeb11
Amélia Alice <alice.cardoso10@gmail.com>
2022-05-02 21:37:27 -0300

    :sparkles: product service
    - add model
    - add repository
    - add controller (not completed)

---
+++ v2/product-service/src/main/java/com/amycardoso/productservice/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.amycardoso.productservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+}
