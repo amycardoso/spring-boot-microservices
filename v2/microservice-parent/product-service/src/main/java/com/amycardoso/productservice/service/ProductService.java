fd1d13e7eef9c677c7c2aa0f8003d343c33a02ad
Amélia Alice <alice.cardoso10@gmail.com>
2022-07-10 17:19:53 -0300

    add parent folder

--- v2/microservice-parent/product-service/src/main/java/com/amycardoso/productservice/service/ProductService.java
+++ v2/microservice-parent/product-service/src/main/java/com/amycardoso/productservice/service/ProductService.java
@@ -29,7 +29,6 @@ public class ProductService {
 
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
-
         return products.stream().map(this::mapToProductResponse).toList();
     }
 
