2a31e0c355afbbc4a5d56b7053f766cac1eb1984
Amélia Alice <alice.cardoso10@gmail.com>
2022-07-26 21:32:55 -0300

    load data

--- v2/inventory-service/inventory-service/src/main/java/com/amycardoso/inventoryservice/InventoryServiceApplication.java
+++ v2/inventory-service/inventory-service/src/main/java/com/amycardoso/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.amycardoso.inventoryservice;
 
+import com.amycardoso.inventoryservice.model.Inventory;
+import com.amycardoso.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +13,21 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("tablet_xps");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("tablet_xps_red");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+        };
+    }
 
 }
